d294021c8993f7918fd57309dfcdbafb32746158
Clément GROHAN <clement.grohan@mes-services.me>
2024-06-01 15:31:49 +0200

    Ajout des typages suite à la création interface

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { Todo } from './interfaces/todo.interface';
 
 @Injectable()
 export class TodosService {
-    todos = [
+    todos: Todo[] = [
         {
             id: 1,
             title: 'Manger des frites',
-            description: 'Il faut manger des frites, mais pas trop souvent.'
+            description: 'Il faut manger des frites, mais pas trop souvent.',
+            done: false,
         },
         {
             id: 2,
             title: 'Acheter du pain',
-            description: 'Penser à acheter du pain.'
+            description: 'Penser à acheter du pain.',
+            done: true,
+        },
+        {
+            id: 3,
+            title: 'Acheter du vin',
+            description: 'Penser à acheter du vin.',
+            done: true,
         }
     ];
 
-    findAl(): any[] {
+    findAl(): Todo[] {
         return this.todos;
     }
 }
